e9915b79cb43e6daad1d2271fb3691b8d17d829d
MatheusHDrum <matheushdrum@gmail.com>
2026-04-30 09:26:53 -0300

    Automação de cálculos
    Antigamente eu jogava um jogo no qual eu trabalhava como mecânico. Cada vez que chegava uma pessoa diferente ela queria coisas diferentes, que são cada uma delas um valor diferente. Criei essa automação que facilita meus cálculos e me envia a soma pronta.

---
+++ Automação Mecânica.py
@@ -0,0 +1,125 @@
+import time
+
+while True:
+    pessoa=str(input("antes de começarmos,seu cliente é uma pessoa comum ou PM?(N/PM)")).upper()
+
+    if pessoa not in ["N", "PM"]:
+        print("não entendi, responda apenas com 'N' ou 'PM'")
+        time.sleep(2)
+    else:
+        break
+
+def performace():
+    print("responda com 'S/N' se os itens foram colocados (ou não) no veículo")
+
+    pp1=str(input("Motor")).upper()
+    pp2=str(input("Cambio")).upper()
+    pp3=str(input("Freios")).upper()
+    pp4=str(input("Suspensão")).upper()
+    pp5=str(input("Blindagem")).upper()
+    pp1 = 1 if pp1=="S" else 0
+    pp2 = 1 if pp2=="S" else 0
+    pp3 = 1 if pp3=="S" else 0
+    pp4 = 1 if pp4=="S" else 0
+    pp5 = 1 if pp5=="S" else 0
+
+    somapp=pp1*50000 + pp2*40000 + pp3*30000 + pp4*25000 + pp5*80000
+    
+    if pessoa=="N":
+        total2=somapp
+        print("a soma deu {}".format(somapp))
+    elif pessoa=="PM":
+         total2=somapp*0.8
+         print("a soma deu {}".format(somapp*0.8))
+    return total2
+
+def estetica():
+    p1=str(input("pintura?(S/N)")).upper()
+    p2=str(input("roda?(S/N)")).upper()
+    p3=int(input("quantas outras peças?"))
+    p1 = 1 if p1=="S" else 0
+    p2 = 1 if p2=="S" else 0
+    soma2= p1*3000 + p2*4000 + p3*2000
+
+   
+    ptotal=str(input("o Veículo dele é nacional, importado ou do governo?(N,I,G")).upper()
+
+    if ptotal=="N":
+        total= soma2
+        print("a soma das tunagens deu {}".format(soma2))
+
+    elif ptotal=="I":
+        total= soma2*1.5
+        print("a soma das tunagens deu {}".format(soma2*1.5))
+
+    elif ptotal=="G":
+        total= soma2*0.8
+        print("a soma das tunagens deu {}".format(soma2*0.8))
+    return total
+
+kit=int(input("Quantos kits?"))
+pneu=int(input("Quantos Pneus?"))              
+chave=int(input("Ele quis quantas chave(s) de roda?"))
+macaco=int(input("e macaco hidráulico, quantos?"))
+soma1=(kit*2500 + pneu*1500 + chave*2000 + macaco*2000)
+if pessoa=="N":
+    print("a soma deu {}".format(soma1))
+elif pessoa=="PM":
+    print("a soma deu {}".format(soma1*0.8))
+
+''
+tunar=str(input("Abrir tunagem? (S/N)")).upper()
+total = 0
+total2 = 0
+if tunar=='S':
+
+    motor=int(input("A pessoa quer tunagem (1)performace ou (2)estética? ou os (3)dois? \
+    (1,2,3)"))
+
+    if motor==1:
+        total2=performace()
+        print("o valor total deu {}".format(total2))
+
+    elif motor==2:
+        total=estetica()
+        print("o valor total deu {}".format(total))
+
+    elif motor==3:
+        moto3=int(input("Vamos começar por onde, (1)performace ou (2)estética?"))
+        if moto3==1:
+            total2=performace()
+            total=estetica()
+        elif moto3==2:
+            total=estetica()
+            total2=performace()
+    print("a soma total deu {}".format(total2+total))
+
+else:
+    print("como decidiu não, acaba por aqui")
+    exit()
+
+"""
+valores tunagem performace:
+motor 50000
+cambio 40000
+blindagem 80000
+Freios 30000
+Suspensao 25000
+
+valores tunagem estetica:
+pintura 3000
+roda (com pintura) 4000
+todo resto 2000 cada
+
+
+Valores itens soltos
+kit reparo= 2500
+pneu= 1500
+chave de roda= 2000
+macaco= 2000
+
+itens soltos por pessoas servidores (policial, bombeiro) -20%
+
+tunagem de veiculos do governo (ambulancia, viatura) -20%
+veiculos importados (porsche, lamborghini) +50%
+"""
